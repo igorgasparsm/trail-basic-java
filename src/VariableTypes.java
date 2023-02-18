e52d3d21f6ce66a63bb8462c352da7713bdc44da
Igo Gaspar <igorgasparsm@gmail.com>
2023-02-18 07:32:09 -0300

    test one

--- src/VariableTypes.java
+++ src/VariableTypes.java
@@ -1,7 +1,8 @@
 public class VariableTypes {
     public static void main(String[] args) {
-        System.out.println("Tenting...");
+        System.out.println("Tenting On...");
         System.out.println("Testing...");
     }
 }
     
+
